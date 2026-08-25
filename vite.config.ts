8a221c2e221526506c4f8f1064dbd24d3cb21326
mhkarami97 <mhkarami97@gmail.com>
2026-08-25 09:48:35 +0330

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       injectRegister: "auto",
-      includeAssets: ["favicon.ico", "favicon.svg", "icons.svg", "apple-touch-icon.png"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
         id: ".",
         name: "Shekaste",
